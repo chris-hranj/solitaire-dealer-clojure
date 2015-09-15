ae50cada813bbbffdeeb1b920eb92c28844fc1c1
chris-hranj <sprtn034@gmail.com>
2015-09-14 21:00:23 -0400

    add comments, nest vector

--- src/solitaire/core.clj
+++ src/solitaire/core.clj
@@ -9,7 +9,7 @@
 (defn shuffle-deck 
   "Shuffle the deck of cards into a random order. Returns a vector of maps"
   [deck-of-cards]
-  (loop [i 51 out [] x (rand-int i) hand deck-of-cards]
+  (loop [i 51 out [] x (rand-int i) hand deck-of-cards] ;;loop from 51 to -1 to avoid IndexOutOfBounds
     (if (not= i -1)
       (recur (dec i) (conj out (get hand x)) (rand-int i) (into (subvec hand 0 x) (subvec hand (inc x))))
       out)))
@@ -17,10 +17,10 @@
 (defn deal 
   "Deal a Solitaire game using a shuffled deck of cards. Returns a double-nested vector."
   [deck-of-cards]
-  (loop [i 1 shuffled-cards deck-of-cards dealt-cards []]
+  (loop [i 1 shuffled-cards deck-of-cards dealt-cards []] ;; loop from 1 to 8 to create 7 piles
     (if (not= i 8)
       (recur (inc i) (subvec shuffled-cards i) (conj dealt-cards (into [] (take i shuffled-cards))))
-      (into [](conj dealt-cards shuffled-cards)))))
+      (vector (conj dealt-cards shuffled-cards))))) ;; add undealt cards to eighth pile of and nest in vector
 
 (defn -main[]
   (println "\nShuffling a Deck...")
